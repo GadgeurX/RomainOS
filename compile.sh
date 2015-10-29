86263ec6e7527456ab20213dab63f5be7de2028c
ardoui_t <theo.ardouin@epitech.eu>
2015-10-29 17:47:02 +0100

    Gitignore updated

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ bcc -0 -ansi -c io.c
 bcc -0 -ansi -c string.c
 bcc -0 -ansi -c fat16.c
 bcc -0 -ansi -c sh.c
-ld86 -d -M main.o io.o string.o fat16.o sh.o -o kernel
-nasm -f bin -o boot boot.asm
-cat boot kernel /dev/zero | dd of=kernel.img bs=512 count=2880
+ld86 -d -M main.o io.o string.o fat16.o sh.o -o kernel.bin
+nasm -f bin -o boot.bin boot.asm
+cat boot.bin kernel.bin /dev/zero | dd of=kernel.img bs=512 count=2880
