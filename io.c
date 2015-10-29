2630add47038e3d9662878d0faeba1ee91d6a392
romainallee <romainallee@gmail.com>
2015-10-29 16:53:09 +0100

    correct io

--- io.c
+++ io.c
@@ -45,12 +45,15 @@ void gets(char *ptr)
 	}
       else if (c == '\b')
 	{
+	  if (i > 0)
+	    {
 	      putc(c);
 	      putc(' ');
 	      putc(c);
 	      i--;
 	      ptr[i] = 0;
 	      i--;
+	    }
 	}
       else
 	{
